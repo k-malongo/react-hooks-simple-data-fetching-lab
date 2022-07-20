fff33dfda0b8745d1201e0793679cf6fd6ceae55
Kelvin <vokemalosh32@gmail.com>
2022-07-20 16:03:51 +0300

    commit tree 096aa9ea973cbd3a9154657800bb880dc8440c3e

--- src/components/App.js
+++ src/components/App.js
@@ -1,21 +1,22 @@
-import React, {useState, useEffect} from 'react'
+import React, { useState, useEffect } from "react";
+
+function App() {
+  const [image, setImage] = useState();
+  const [isLoaded, setIsLoaded] = useState(false);
 
 
-function App(){
-    const [displayDog, setDisplayDog] = useState([]);
-    const [isLoaded, setIsLoaded] = useState(false);
+  useEffect(() => {
+    fetch("https://dog.ceo/api/breeds/image/random")
+      .then((response) => response.json())
+      .then((data) => {
+    setImage(data.message)
+    setIsLoaded(true)
+});
+  }, []);
+  // use an empty dependencies array, so we only run the fetch request ONCE
+  if (!isLoaded) return <p>Loading...</p>;
 
-    useEffect(() => {
-        fetch("https://dog.ceo/api/breeds/image/random")
-          .then((response) => response.json())
-          .then((data) => {
-            console.log(data)
-            setDisplayDog(data.message);
-            setIsLoaded(true);
-          });
-      },
-       []);
-       if (!isLoaded) return <p>Loading...</p>;
-       return <div><img src={displayDog} alt ="dogCeo" /></div>
+  return <div> <img src={image} alt="A Random Dog" /> </div>;
 }
+
 export default App;
